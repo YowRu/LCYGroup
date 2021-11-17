33efc4f2761de58c123ced65522fef1b0ea17529
Angela Yu <love_3221213@hotmail.com>
2021-11-17 10:50:14 +0800

    fixed article layout

--- js/articleNew.js
+++ js/articleNew.js
@@ -2,10 +2,13 @@ $(document).ready(function(){
   let w = $('#main img.top').width();
   $('#main p').width(w);
   $('#main .border').outerWidth(w);
-//resize
-$(window).resize(function(){
+});
   let w = $('#main img.top').width();
   $('#main p').width(w);
   $('#main .border').outerWidth(w);
-  });
-});
+//resize
+  $(window).resize(function(){
+    let w = $('#main img.top').width();
+    $('#main p').width(w);
+    $('#main .border').outerWidth(w);
+    });
